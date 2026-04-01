870461cd90eb1eda5aced80b4248536251ff88eb
Jordao Asato <jordaovolletasato@gmail.com>
2026-04-01 08:54:45 -0300

    Projeto criado

---
+++ src/main/java/com/workshop/xbrainvendas/XbrainvendasApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.xbrainvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbrainvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XbrainvendasApplication.class, args);
+	}
+
+}
