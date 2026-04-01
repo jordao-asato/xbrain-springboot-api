7228dfbb22f733409415ed25493d846f6763cb64
Jordao Asato <jordaovolletasato@gmail.com>
2026-04-01 10:16:10 -0300

    H2, perfil de teste e JPA

--- src/main/java/com/workshop/xbrainvendas/entities/Seller.java
+++ src/main/java/com/workshop/xbrainvendas/entities/Seller.java
@@ -3,9 +3,19 @@ package com.workshop.xbrainvendas.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_seller")
 public class Seller implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY) // auto-incremento
 	private Long id;
 	private String name;
 	
